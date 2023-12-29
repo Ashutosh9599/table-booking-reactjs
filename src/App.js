efd17b722c3e8ec18f4003ec93a88e0b999f9d46
Ashutosh9599 <ashutoshdas091@gmail.com>
2023-12-29 16:06:13 +0530

    test Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Form from './components/Form/Form';
+import List from './components/List/List';
 
 function App() {
+  const [orders, setOrders] = useState({});
+
+  const fetchOrdersFromLocalStorage = () => {
+    const storedOrders = {};
+    Object.keys(localStorage).forEach((key) => {
+      if (key !== 'orders') {
+        storedOrders[key] = JSON.parse(localStorage.getItem(key));
+      }
+    });
+    return storedOrders;
+  };
+
+  useEffect(() => {
+    const storedOrders = fetchOrdersFromLocalStorage();
+    setOrders(storedOrders);
+  }, []); // Empty dependency array ensures it runs only once on mount
+
+  const handleSubmit = (data) => {
+    const { orderId, selectedTable } = data;
+    const updatedOrders = {
+      ...orders,
+      [orderId]: { ...data, table: selectedTable },
+    };
+    setOrders(updatedOrders);
+
+    localStorage.setItem(orderId, JSON.stringify({ ...data, table: selectedTable }));
+  };
+
+  const handleDelete = (orderId) => {
+    const updatedOrders = { ...orders };
+    delete updatedOrders[orderId];
+
+    setOrders(updatedOrders);
+    localStorage.removeItem(orderId);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form onSubmit={handleSubmit} />
+      <List orders={orders} onDelete={handleDelete} />
     </div>
   );
 }
